960426ce4890d97bfa7f8bc3eb943b334d1fcc9d
Vanina Castelo <v.i.castelo@gmail.com>
2023-08-16 12:42:00 -0700

    Realizo primer commit

---
+++ primer-parcial-FEII/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card'
+
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+
+    <Card/>
+    
+  </div>
+  )
+}
+
+export default App
