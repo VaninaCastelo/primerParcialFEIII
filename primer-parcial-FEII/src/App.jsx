e166318f24d9e70fbecbf3f9ea279a58b854cc5d
Vanina Castelo <v.i.castelo@gmail.com>
2023-08-16 18:39:23 -0700

    Actualizo Proyecto Parcial

--- primer-parcial-FEII/src/App.jsx
+++ primer-parcial-FEII/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <div className='App'>
-      
-    <h1>Carga de estudiantes</h1>
-    <form></form>
-
+    <h1>TAYLOR SWIFT PARTY</h1>
+    <h2>The Eras Tour Edition</h2>
+    <br />
+    <p>Estamos confeccionando el playlist de la siguiente TS party, y queremos saber que canciones son tu MUST BE que no te pueden faltar</p>
     <Card/>
     
   </div>
--- primer-parcial-FEII/src/components/Card.jsx
+++ primer-parcial-FEII/src/components/Card.jsx
@@ -1,8 +1,46 @@
-import React from "react";
-
+// import React from "react";
+import React, { useState } from 'react'
 
 const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(false)
+  const [participante, setParticipante] = useState({
+        nombre: '',
+        cancion: '',
+    })
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault()
+        if (participante.nombre.length >= 3 && participante.cancion.length >= 6)
+    { setShow(true)
+    setError(false)
+    } else 
+    setError (true)
+  }
+
+  const handleChange = (event) => setParticipante({...participante, nombre: event.target.value})
+  console.log(participante)
+  return (
+    <form className="form-container">
+        <label htmlFor="Nombre">Dinos tu Nombre</label>
+        <br />
+        {/* <input type="text" onChange={(event)=> setParticipante({...participante, nombre: event.target.value})}/> */}
+        <input type="text" onChange={handleChange}/>
+        <br />
+        <label htmlFor="Cancion">Ingresa tu cancion favorita con su disco correspondiente</label>
+        <br />
+        <input type="text" onChange={(event)=> setParticipante({...participante, cancion: event.target.value})}/>
+        <br />
+        <br />
+        <button onClick= {handleSubmit} > ESTA NO ME PUEDE FALTAR ! </button>
+  {error && <h6 style={{ color: 'red' }}>Por favor chequea que la información sea correcta</h6> }
+  {show ? <> <h3 style={{ color: 'magenta' }}> Gracias {participante.nombre} </h3>
+            <h6 style={{ color: 'violet' }}> Agregaremos ¨ {participante.cancion} ¨ a nuestra playlist </h6> </>
+            : null }
+
+    </form>
+  )
 };
 
 
